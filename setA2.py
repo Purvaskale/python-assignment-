8b727c64b2a51ba90a1cfb9f15d15514aec68a6e
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 19:03:03 +0530

    Add files via upload

---
+++ setA2.py
@@ -0,0 +1,17 @@
+Que.python program to swap two variable.
+program:-
+
+ n=int(input("Enter no1:"))
+ m=int(input("Enter no2:"))
+ print("\nBefore Swap:n=",n,"m=",m)
+ temp=n
+ n=m
+ m=temp
+ print("\nAfter Swap:n=",n,"m=",m)
+
+output:-
+
+Enter no1:56	
+Enter no2:89
+Before Swap:n= 56 m= 89
+After Swap:n= 89 m= 56
