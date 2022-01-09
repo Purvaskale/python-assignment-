1eb8ce6b3dfbcbe8769a3f3cbc1844ab656dd705
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 20:11:56 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,11 @@
+Que.write python program to convert kilometer to miles.
+program:-
+
+k=float(input("enter value in kilometer"))
+m=k*0.62137
+print("Miles=",m)
+
+output:-
+
+enter value in kilometer3
+Miles= 1.86411
