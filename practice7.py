e27629a0abeb00727b9b9663ca1a84cf73d5b511
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 21:01:36 +0530

    Add files via upload

---
+++ practice7.py
@@ -0,0 +1,13 @@
+Que.write python program to find sum of natural number.
+program:-
+
+a=int(input("enter limit:"))
+s=0
+for i in range(1,a+1):
+    s=s+i
+print("sum of Natural Number:",s)
+
+
+output:-
+enter limit:7
+sum of Natural Number: 28
