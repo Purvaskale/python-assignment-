7ce7006a65ed51e1ab98578213e5b17205027b71
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 20:04:17 +0530

    Add files via upload

---
+++ setB5.py
@@ -0,0 +1,13 @@
+Que.python program to find factorial number 
+program:-
+
+fact=1
+n=int(input("enter no:"))
+for i in range(2,n):
+    fact=fact*i
+print("Factorial of num:",fact)
+
+output:-
+
+enter no:6
+Factorial of num: 120
