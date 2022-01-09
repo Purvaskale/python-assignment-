66c4f7840162e5b9893313ad8c994caa5d96ec66
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 20:16:14 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,11 @@
+Que.write python program to convert celcius to farenheit.
+program:-
+
+c=int(input("enter value celcius:"))
+f=(c*9/5)+32
+print("Farenheit=",f)
+
+output:-
+
+enter value celcius:32
+Farenheit= 89.6
