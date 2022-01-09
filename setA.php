5d42eab3cda46eb492d543d4d38ff9c0d7ddbd9c
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 18:45:28 +0530

    Add files via upload

---
+++ setA.php
@@ -0,0 +1,10 @@
+Que .python program to calculate area of traigle
+
+program:-	b=int(input("Enter Breadth:"))
+	h=int(input("Enter Height:"))
+	area=int(1/2*b*h)
+	print("area of traingle=",area)
+
+output:-	Enter Breadth:6
+	Enter Height:7
+	area of traingle= 21
