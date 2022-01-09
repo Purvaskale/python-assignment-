3db164423cc129d9e2050f73c6cf1c38bab308fc
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 19:28:33 +0530

    Add files via upload

---
+++ setB2.py
@@ -0,0 +1,13 @@
+Que.python program to check if a number is Even or Odd.
+program:-
+
+ p=int(input("Enter no:"))
+if p%2==0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+output:-
+
+Enter no:46
+Number is Even
