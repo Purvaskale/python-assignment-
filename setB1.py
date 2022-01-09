37168df45bf9769f61944c59716b9eb6ce4dea5e
Purvaskale <96783054+Purvaskale@users.noreply.github.com>
2022-01-09 19:22:36 +0530

    Add files via upload

---
+++ setB1.py
@@ -0,0 +1,15 @@
+Que.python program to to check if a number is positive,negative,zero
+program:-
+
+ p=int(input("Enter no:"))
+if p>0:
+    print("Number is Positive")
+elif p<0:
+    print("Number is Negative")
+else:
+    print("Number is zero")
+
+output:-
+
+Enter no:99
+Number is Positive
